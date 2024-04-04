62c73aee4c33db50eba8bb728d0b744cc30a46f1
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-04-04 11:26:50 +0200

    initial commmit

---
+++ src/main/java/se/vandingenen/MooWebApplication.java
@@ -0,0 +1,13 @@
+package se.vandingenen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/vandingenen/MooWebApplicationTests.java
@@ -0,0 +1,13 @@
+package se.vandingenen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
