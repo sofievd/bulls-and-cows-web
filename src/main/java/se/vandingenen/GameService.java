fc8b0d7b96662108c42ec47211e76c63fbc1206c
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-04-19 11:47:59 +0200

    Added a class to hold a guess, with the number of guess it was, the guess itself and what result it gave.

--- src/main/java/se/vandingenen/GameService.java
+++ src/main/java/se/vandingenen/GameService.java
@@ -1,10 +1,13 @@
 package se.vandingenen;
 
+import com.sun.source.tree.Tree;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.HashMap;
+import java.util.*;
 
 @Service
 @SessionScope
@@ -12,5 +15,50 @@ public class GameService{
     @Autowired
     BullsAndCowsRules rules;
 
+    private ArrayList<GuessItem> guessResultNumofGueses;
+    private String secret;
 
+    @PostConstruct
+    public void newGame(){
+        guessResultNumofGueses = new ArrayList<>();
+        secret = rules.getSecret();
+    }
+
+    public String makeGuess(String guess){
+        GuessEvaluation evaluation = rules.checkResult(guess);
+        String result = rules.showResult(evaluation);
+        addResultAndGuess(rules.getNumberOfGuesses(), guess, result);
+        if(rules.isFinished(evaluation)){
+            return "wrong, try again!";
+        }else {
+            return "correct!";
+        }
+    }
+
+    public boolean finished(String resultToShow){
+        if(!resultToShow.equalsIgnoreCase("correct!")){
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+    public void startNewGame(){
+        guessResultNumofGueses.clear();
+        newGame();
+    }
+    public void addResultAndGuess(int numofGuesses, String guess, String result){
+        GuessItem guessItem = new GuessItem(numofGuesses, guess, result);
+        guessResultNumofGueses.add(guessItem);
+
+    }
+
+    public ArrayList<GuessItem> getGuessResultNumofGueses() {
+        return guessResultNumofGueses;
+    }
+
+    public void setGuessResultNumofGueses(ArrayList<GuessItem> guessResultNumofGueses) {
+        this.guessResultNumofGueses = guessResultNumofGueses;
+    }
 }
---
+++ src/main/java/se/vandingenen/GuessItem.java
@@ -0,0 +1,37 @@
+package se.vandingenen;
+
+public class GuessItem {
+    private int numOfGuesses;
+    private String guess;
+   private String result;
+
+    public GuessItem(int numOfGuesses, String guess, String result) {
+        this.numOfGuesses = numOfGuesses;
+        this.guess = guess;
+        this.result = result;
+    }
+
+    public int getNumOfGuesses() {
+        return numOfGuesses;
+    }
+
+    public void setNumOfGuesses(int numOfGuesses) {
+        this.numOfGuesses = numOfGuesses;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
